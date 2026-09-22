f037752a939c6f95365fe9a477ad42143465cd24
Bhargavi Mudragiri <bhargavimudragiri31@gmail.com>
2026-09-22 17:52:03 +0100

    Automate EKS OIDC and EBS CSI setup with Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -171,3 +171,61 @@ resource "aws_eks_node_group" "petclinic" {
     Name = "${var.project_name}-node-group"
   }
 }
+
+# EKS OIDC Provider
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.petclinic.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.petclinic.identity[0].oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+}
+
+# IAM Role for EBS CSI Driver
+resource "aws_iam_role" "ebs_csi" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "${replace(aws_eks_cluster.petclinic.identity[0].oidc[0].issuer, "https://", "")}:aud" = "sts.amazonaws.com"
+            "${replace(aws_eks_cluster.petclinic.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+# EBS CSI Driver Add-on
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.petclinic.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi.arn
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ebs_csi_policy,
+    aws_eks_node_group.petclinic
+  ]
+}
